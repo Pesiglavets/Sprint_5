39bdb396190c2b42033b99a6ae62bf68b09974e1
Pesiglavets <russdebut8@gmail.com>
2025-09-14 22:47:44 +0300

    deleted extra tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -18,27 +18,3 @@ class TestLogin:
 
         assert page.is_element_visible(MainPageLocators.USER_AVATAR)
         assert page.is_element_visible(MainPageLocators.USER_NAME)
-
-    @pytest.mark.negative
-    def test_login_wrong_password_shows_error_message(self, driver, base_url, registered_user):
-        driver.get(base_url)
-        page = BasePage(driver)
-        page.click(MainPageLocators.LOGIN_BUTTON)
-        page.send_keys(AuthPageLocators.EMAIL_INPUT, registered_user["email"])
-        page.send_keys(AuthPageLocators.PASSWORD_INPUT, "WrongPassword123")
-        page.click(AuthPageLocators.LOGIN_BUTTON)
-        
-        assert page.is_element_visible(AuthPageLocators.WRONG_LOGIN_PASSWORD_MESSAGE)
-        assert not page.is_element_visible(MainPageLocators.USER_AVATAR)
-
-    @pytest.mark.negative
-    def test_login_nonexistent_email_shows_error_message(self, driver, base_url, standard_password):
-        driver.get(base_url)
-        page = BasePage(driver)
-        page.click(MainPageLocators.LOGIN_BUTTON)
-        page.send_keys(AuthPageLocators.EMAIL_INPUT, "nonexistent@test.com")
-        page.send_keys(AuthPageLocators.PASSWORD_INPUT, standard_password)
-        page.click(AuthPageLocators.LOGIN_BUTTON)
-        
-        assert page.is_element_visible(AuthPageLocators.WRONG_LOGIN_PASSWORD_MESSAGE)
-        assert not page.is_element_visible(MainPageLocators.USER_AVATAR)
